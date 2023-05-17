317698cd60175a6f9addba8c2ab5deb109809704
Herilala R <herilalah.rakotonasolo@gmail.com>
2023-05-17 15:14:56 +0300

    Testing user registration

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -2,18 +2,45 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Support\Str;
+use Database\Factories\UserFactory;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
 
-class AuthenticationTest extends TestCase
+class Authentication extends TestCase
 {
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
+    public function test_user_can_register()
     {
-        $response = $this->get('/');
+
+        //Create
+        $password = Hash::make('password');
+
+        $user = [
+            'name' => fake()->name,
+            'pseudo' => fake()->firstName(),
+            'phone_number' => Str::random(10),
+            'password' => $password,
+            'password_confirmation' => $password
+        ];
+
+        //Action
+        $response = $this->post('/api/register', $user);
+
+        //Assertion
+        $response->assertStatus(201);
+    }
+
+    public function test_user_can_login()
+    {
+        $user = User::first()->only(['pseudo', 'password']);
+
+        $response = $this->post('/api/login', $user);
 
         $response->assertStatus(200);
     }
