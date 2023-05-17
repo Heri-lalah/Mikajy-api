fee214ff63e69d53723dc34c54df875193c12423
Herilala R <herilalah.rakotonasolo@gmail.com>
2023-05-17 15:15:25 +0300

    Testing user registration

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -5,12 +5,11 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use App\Models\User;
 use Illuminate\Support\Str;
-use Database\Factories\UserFactory;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Hash;
 
-class Authentication extends TestCase
+class AuthenticationTest extends TestCase
 {
     /**
      * A basic feature test example.
